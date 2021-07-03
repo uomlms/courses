12dfbe1293e7271211d48fb6978bae19509237aa
OrestisNer <orestis@ep-mail.gr>
2021-07-03 04:01:36 +0300

    Create Course Model

---
+++ src/models/courses.ts
@@ -0,0 +1,60 @@
+import mongoose from 'mongoose';
+
+
+interface CourseAttrs {
+  name: string;
+  description: string;
+  semester: number;
+  status?: string;
+}
+
+interface CourseDoc extends mongoose.Document {
+  name: string;
+  description: string;
+  semester: number;
+  status?: string;
+}
+
+interface CourseModel extends mongoose.Model<CourseDoc> {
+  build(attrs: CourseAttrs): CourseDoc;
+}
+
+const CourseSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  semester: {
+    type: Number,
+    required: true
+  },
+  status: {
+    type: String,
+    enum: ["active", "inactive"],
+    default: "active"
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+}, {
+  toJSON: {
+    transform(doc, ret) {
+      ret.id = ret._id;
+      delete ret._id;
+      delete ret.__v;
+    }
+  }
+});
+
+CourseSchema.statics.build = (attrs: CourseAttrs) => {
+  return new Course(attrs);
+}
+
+const Course = mongoose.model<CourseDoc, CourseModel>('Course', CourseSchema);
+
+export { Course }
