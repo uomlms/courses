f3171fdeae9ee1167830c4a179df05236ca92578
OrestisNer <orestis@ep-mail.gr>
2021-07-06 20:53:00 +0300

    Remove json from body parser and use it from express

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,5 @@
 import express from 'express';
 import 'express-async-errors';
-import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
 import { errorHandler, NotFoundError, currentUser } from '@uomlms/common';
 import { createCourseRouter } from './routes/courses/create';
@@ -9,13 +8,15 @@ import { updateCourseRouter } from './routes/courses/update';
 
 const app = express();
 app.set('trust proxy', true);
-app.use(json());
+app.use(express.json());
+
 app.use(
   cookieSession({
     signed: false,
     secure: process.env.NODE_ENV !== 'test',
   })
 );
+
 app.use(currentUser);
 
 app.use(createCourseRouter);
