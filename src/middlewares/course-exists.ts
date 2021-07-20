a5e3ded0b40c5f9c56ff8c818d80c249b1913229
OrestisNer <orestis@ep-mail.gr>
2021-07-20 20:29:52 +0300

    Course Exists middleware saves the course in request object

--- src/middlewares/course-exists.ts
+++ src/middlewares/course-exists.ts
@@ -2,6 +2,24 @@ import { Request, Response, NextFunction } from 'express';
 import { NotFoundError } from '@uomlms/common';
 import { Course } from '../models/courses';
 
+
+// defines the payload of the course
+interface CoursePayload {
+  name: string;
+  description: string;
+  semester: number;
+  status?: string;
+}
+
+// declares an extra attribute to Request Object
+declare global {
+  namespace Express {
+    interface Request {
+      course?: CoursePayload
+    }
+  }
+}
+
 export const courseExists = async (
   req: Request,
   res: Response,
@@ -10,7 +28,9 @@ export const courseExists = async (
   const courseId = req.params.courseId;
   const course = await Course.findById(courseId);
   if (!course) {
-    throw new NotFoundError()
+    throw new NotFoundError();
   }
+
+  req.course = course;
   next();
 }
