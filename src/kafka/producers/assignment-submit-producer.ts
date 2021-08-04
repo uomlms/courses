df376bec1dcaa930ce56591607c01371c58602c4
Orestis <orestis@ep-mail.gr>
2021-08-05 00:23:49 +0300

    Convert topic to readonly property at AssignmentSubmitProducer

--- src/kafka/producers/assignment-submit-producer.ts
+++ src/kafka/producers/assignment-submit-producer.ts
@@ -1,5 +1,5 @@
 import { Producer, Topics, AssignmentSubmitEvent } from '@uomlms/common';
 
 export class AssignmentSubmitProducer extends Producer<AssignmentSubmitEvent> {
-  topic = Topics.AssignmentSubmitTopic;
+  readonly topic = Topics.AssignmentSubmitTopic;
 }
