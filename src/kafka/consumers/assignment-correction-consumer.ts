ed54a03252e2a4abdf2282cf96682f75815e2911
Orestis <orestis@ep-mail.gr>
2021-08-10 22:10:22 +0300

    Fix spaces

--- src/kafka/consumers/assignment-correction-consumer.ts
+++ src/kafka/consumers/assignment-correction-consumer.ts
@@ -3,18 +3,18 @@ import { Message } from 'node-rdkafka';
 import { Submission } from '../../models/submission';
 
 export class AssignmentCorrectionConsumer extends Consumer<AssignmentCorrectionEvent> {
-    readonly topic = Topics.AssignmentCorrectionTopic;
+  readonly topic = Topics.AssignmentCorrectionTopic;
 
-    async onMessage(data: AssignmentCorrectionEvent['data'], message: Message) {
-        const submission = await Submission.findById(data.submissionId);
-        if (!submission) { throw new NotFoundError(); }
+  async onMessage(data: AssignmentCorrectionEvent['data'], message: Message) {
+    const submission = await Submission.findById(data.submissionId);
+    if (!submission) { throw new NotFoundError(); }
 
-        submission.set({
-            status: data.status,
-            result: data.result
-        });
+    submission.set({
+      status: data.status,
+      result: data.result
+    });
 
-        await submission.save();
+    await submission.save();
 
-    }
+  }
 }
